259166f37e189576ac0f8c2c9ebdc067d2a97491
ASTRAY TEAM <86219387+skar80@users.noreply.github.com>
2022-01-01 21:18:55 +0300

    Add files via upload

---
+++ install-termux-metasploit.sh
@@ -0,0 +1,67 @@
+ #!/bin/bash
+clear
+echo 
+echo  I am Ahmed. This script helps to install a tool metasploit
+echo #############################################################
+	
+echo  If you have a problem with me
+echo My social communication accounts
+echo ########################################################
+
+toilet -F gay "The speed"
+figlet ahmd sami
+#############################################################
+
+echo  inst:0ham1
+echo   Telegram:z0hma
+echo YouTube:https://youtube.com/channel/UC9lZ1luKm9i5986DHmbLG9w
+echo #############################################################
+ 
+apt update
+termux-setup-storage
+pkg install python
+pkg install python2
+pkg install git
+pkg install ruby
+pkg install nmap
+pkg install unzip
+pkg install nano
+apt install php
+apt install golang
+apt install nano
+apt install cmatrix
+pkg install figlet
+pkg install wget
+pkg install cowsay
+pkg install toilet
+pkg install curl
+pkg install openssh
+pkg install tor
+pkg install net-tools
+pkg install unrar
+pkg install clang
+pkg install w3m
+pkg install proot
+pip2 install wget
+pip2 install requests
+apt update
+apt upgrade
+##################################################################
+clear
+pkg update && pkg upgrade -y
+pkg install wget curl openssh git -y
+apt install ncurses-utils
+wget https://raw.githubusercontent.com/gushmazuko/metasploit_in_termux/master/metasploit.sh
+
+ls
+* chmod+x
+./metasploit.sh
+
+./postgresql_ctl.sh 
+
+cd metasploit-framework
+ls
+./msfconsole
+
+###################################################################
+toilet -F gay "The end"
