4020abf5203ca58f999228e611a7e891af3e3041
ASTRAY TEAM <86219387+skar80@users.noreply.github.com>
2022-01-01 22:19:32 +0300

    Add files via upload

---
+++ the-speed.sh
@@ -0,0 +1,19 @@
+#!bin/bash
+
+toilet - F gay "The-Speed"
+
+pkg update && pkg upgrade -y
+pkg install wget curl openssh git -y
+apt install ncurses-utils
+wget https://raw.githubusercontent.com/gushmazuko/metasploit_in_termu>
+
+ls
+* chmod+x
+./metasploit.sh
+
+./postgresql_ctl.sh
+
+cd metasploit-framework
+ls
+./msfconsole
+
